27316d0eab4eb7a6fb00c3fe6304880b068cb095
prajapatiurvi977 <prajapatiurvi977@gmail.com>
2018-06-23 19:53:43 +0530

    server.js commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-var base_url = "D:/pizza_order/Pzza_order";
+$(document).ready(function(){
+	var base_url = "D:/pizza_order/Pzza_order";
+	getPizza();
+	function getPizza(){
+		$.get("server/pizzas.json", function(data, status){
+			alert("Data: " + data + "\nStatus: " + status);
+		});
+	}
+   
+
+});
